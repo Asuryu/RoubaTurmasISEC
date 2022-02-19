c104b4a5497bb05dc5ca294a4cf1dc15b361be5e
M47Z <38143854+IamM47Z@users.noreply.github.com>
2022-02-19 21:54:14 +0000

    fast coding
    so falta a funçao para inscrever na turma e deixar o codigo mais bonito

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
 import time
 import json
 
@@ -30,8 +33,42 @@ def pedeCredenciais(nr_aluno):
     
     return password
 
+def openTab(driver):
+    old_tab = driver.current_window_handle
+    driver.execute_script("window.open(\"\", \"_blank\");")
+    driver.switch_to_window(driver.window_handles[-1])
+    return old_tab
 
+def closeTab(driver, new_tab):
+    driver.execute_script("window.close();")
+    driver.switch_to_window(new_tab)
+    return
 
+def isValidNonioPage( driver ):
+    try:
+        WebDriverWait( driver, 10 ).until( EC.presence_of_element_located( ( By.ID, "flxContainer" ) ) )
+        return True
+    except:
+        return False
+
+def validPage( driver ):
+    for i in range( 5 ):
+        if isValidNonioPage( driver ):
+            break
+
+        print( "Invalid Page, refreshing." )
+        time.sleep( 5 )
+        driver.refresh( )
+
+    if not isValidNonioPage( driver ):
+        print( "Oops, we got a problem here, check your internet and the website and try again later" )
+        time.sleep( 5 )
+        driver.quit( )
+        exit( 1 )
+
+def signClass( driver, class_name ):
+    # codigo por desvendar
+    return True
 
 mostraASCII()
 data = lerFicheiro()
@@ -42,21 +79,51 @@ driver = webdriver.Firefox()
 driver.get("https://inforestudante.ipc.pt/nonio/security/login.do")
 assert "InforEstudante - NONIO IPC" in driver.title
 
-email_input = driver.find_element("id", "username")
+email_input = driver.find_element(By.ID, "username")
 email_input.click()
 email = "a" + data["numero_aluno"] + "@isec.pt"
 for letter in email:
     email_input.send_keys(letter)
     
-password_input = driver.find_element("id", "password1")
+password_input = driver.find_element(By.ID, "password1")
 password_input.click()
 for letter in password:
     password_input.send_keys(letter)
 
-login = driver.find_element("class name", "button")
+login = driver.find_element(By.CLASS_NAME, "button")
 login.click()
 
-time.sleep(10)
-driver.close()
+if not isValidNonioPage( driver ):
+    print( "Error logging in. Please check your credentials and try again." )
+    time.sleep( 5 )
+    driver.quit( )
+    exit( 1 )
 
-#driver.get("https://inforestudante.ipc.pt/nonio/inscturmas/listaInscricoes.do")
+driver.get("https://inforestudante.ipc.pt/nonio/inscturmas/listaInscricoes.do")
+
+validPage( driver )
+
+subject_table = driver.find_element(By.ID, "displaytable").find_element_by_tag_name( "tbody" )
+for subject_row in subject_table.find_elements_by_css_selector( "tr" ):
+    for subject_info in data[ "cadeiras" ]:
+        if not subject_info[ "nome" ] in subject_row.text:
+            continue
+        
+        link = subject_row.find_element_by_class_name( "botaodetalhes" ).href
+        original_tab = openTab( driver )
+
+        driver.get( link )
+
+        validPage( driver )
+
+        for subject_class in subject_info[ "teorica" ]:
+            if signClass(driver, subject_class):
+                break
+        
+        for subject_class in subject_info[ "pratica" ]:
+            if signClass(driver, subject_class):
+                break
+
+        # submit btn se bem me lembro
+
+        closeTab( driver, original_tab )
